48780e85a3d96b41fce27e60239c374df392f465
MooN <abdulrhmanbawzir@gmail.com>
2022-08-01 02:53:22 +0300

    update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-        <span class="iconify" data-icon="bi:code-slash"></span> with <span role="img">❤️</span> by {greeting.title}
+        <span class="iconify" data-icon="bx:code-alt"></span> with <span role="img">❤️</span> by {greeting.title}
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
