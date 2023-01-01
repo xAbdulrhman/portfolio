2c642f78837a441ed1549ca3a31ec13da278849d
MooN <abdulrhmanbawzir@gmail.com>
2023-01-01 23:31:09 +0300

    sm

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -8,7 +8,7 @@ import { experience } from "../../portfolio.js";
 import { Fade } from "react-reveal";
 import ExperienceImg from "./ExperienceImg";
 
-const experience = {
+ experience = {
 	title: "Experience",
 	subtitle: "Work, Internship and Volunteership",
 	description:
