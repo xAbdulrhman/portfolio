6da151a1fd1b36f1dc53e0752da5c64bfed26b4e
MooN <abdulrhmanbawzir@gmail.com>
2023-01-07 17:18:44 +0300

    c

--- src/portfolio.js
+++ src/portfolio.js
@@ -417,7 +417,7 @@ const certifications = {
             logo_path: "CBP.png",
             certificate_link: "https://drive.google.com/file/d/1Bvhp78LS-Em4fF6NHxTcuafdo8SPbgwC/view?usp=sharing",
             alt_name: "CBP-CT",
-            color_code: "#26C33391",
+            color_code: "#21C31391",
         },
         {
             title: "Organizational IQ",
